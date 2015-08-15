3891a651b561525d09ec0633fcf7bf5bf298bdc0
@djghostghost <xuxi746@163.com>
2015-08-15 22:01:47 +0900

    leetcode #53 is added

--- src/org/practise/DynamicProgramming/DynamicProgramming.java
+++ src/org/practise/DynamicProgramming/DynamicProgramming.java
@@ -37,11 +37,89 @@ public class DynamicProgramming {
 
 
     }
-    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid==null || obstacleGrid.length==0){
+            return 0;
+        }
 
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+
+        int[][] paths=new int[m][n];
+        paths[0][0]=1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    paths[i][j]=0;
+                }else{
+                    if(j!=0) paths[i][j]+=paths[i][j-1];
+                    if(i!=0) paths[i][j]+=paths[i-1][j];
+                }
+
+            }
+
+        }
+        return paths[m-1][n-1];
+    }
+
+    //leetcode 64
+    public int minPathSum(int[][] grid) {
+        if(grid==null || grid.length==0){
+            return 0;
+        }
+
+        int m=grid.length;
+        int n=grid[0].length;
+
+
+        int[][] paths=new int[m][n];
+        paths[0][0]=grid[0][0];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i!=0 && j!=0){
+                    paths[i][j]=Math.min(paths[i-1][j],paths[i][j-1])+grid[i][j];
+                }else if(i==0 && j!=0){
+                    paths[i][j]=paths[i][j-1]+grid[i][j];
+                }else if(j==0 && i!=0){
+                    paths[i][j]=paths[i-1][j]+grid[i][j];
+                }
+
+
+            }
+        }
+
+        return paths[m-1][n-1];
+
+    }
+
+//    leetcode #53
+    public int maxSubArray(int[] nums) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+
+
+
+        int max=nums[0];
+        int currMax=nums[0];
+        int len=nums.length;
+        for(int i=1;i<len;i++){
+            currMax=Math.max(currMax,0)+nums[i];
+            max=Math.max(currMax,max);
+        }
+
+        return max;
+    }
     public static void main(String[] args) {
         DynamicProgramming sol=new DynamicProgramming();
-        System.out.println(sol.uniquePaths(3,2));
+
+        int nums[]={2};
+
+        System.out.println(sol.maxSubArray(nums));
+
+
     }
 
 }
