bb03526c45d7fbedc5cc0dd0b3f3c834180b639f
djghostghost <xuxi746@163.com>
2015-08-25 23:32:36 +0900

    leetcode #264 is added

--- src/org/practise/math/UglyNumber.java
+++ src/org/practise/math/UglyNumber.java
@@ -1,5 +1,7 @@
 package org.practise.math;
 
+import java.util.Stack;
+
 /**
  * Created by ghost on 2015/8/21.
  */
@@ -26,9 +28,44 @@ public class UglyNumber {
 
 
     }
+    //leetcode 264
+    public int nthUglyNumber(int n) {
+        if(n<1){
+            return 0;
+        }
 
-    public static void main(String[] args) {
+        int[] results=new int[n];
+        int id2=0,id3=0,id5=0,res=1;
+        results[0]=res;
+        for(int i=1;i<n;i++){
+
+
+            int v2=results[id2]*2;
+            int v3=results[id3]*3;
+            int v5=results[id5]*5;
+
+            res=Math.min(Math.min(v2,v3),v5);
+
+
+            if(res==v2){
+                id2++;
+            }
+            if(res==v3){
+                id3++;
+            }
+            if(res==v5){
+                id5++;
+            }
+            results[i]=res;
+
+        }
+
+        return res;
+
+    }
+
+        public static void main(String[] args) {
         UglyNumber sol=new UglyNumber();
-        System.out.println(sol.isUgly(0));
+        System.out.println(sol.nthUglyNumber(10));
     }
 }
