d1f12e18efc24543e6c797de04398b0eeb558b56
@djghostghost <xuxi746@163.com>
2015-09-19 22:20:10 +0900

    added leetcode #283

---
+++ src/org/practise/array/MoveZeros.java
@@ -0,0 +1,44 @@
+package org.practise.array;
+
+import sun.jvmstat.perfdata.monitor.MonitorVersionException;
+
+/**
+ * Created by liguoxiang on 9/19/15.
+ */
+//leetcode #283
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+
+        int zeroIndex=0;
+        int elementIndex=0;
+
+        for(;elementIndex<nums.length;elementIndex++){
+
+            if(zeroIndex!=elementIndex && nums[elementIndex]!=0){
+                nums[zeroIndex]=nums[elementIndex];
+                zeroIndex++;
+            }else if(zeroIndex==elementIndex && nums[elementIndex]!=0){
+                zeroIndex++;
+            }
+
+        }
+
+
+        for(;zeroIndex<nums.length;zeroIndex++){
+            nums[zeroIndex]=0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MoveZeros sol=new MoveZeros();
+        int[] nums={0,0,0,0};
+        sol.moveZeroes(nums);
+
+        for(int num:nums){
+            System.out.print(num+"->");
+        }
+
+    }
+
+}
