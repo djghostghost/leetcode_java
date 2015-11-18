30f92e8b041240c179ea4d8314004c76a674f1a8
djghostghost <xuxi746@163.com>
2015-11-18 22:01:47 +0900

    fixed leetcode #39 added leetcode #40

--- src/org/practise/backtracking/CombinationSum.java
+++ src/org/practise/backtracking/CombinationSum.java
@@ -24,9 +24,22 @@ public class CombinationSum {
         }
         Arrays.sort(candidates);
         int len=candidates.length-1;
+
+        int i=len;
+        for(;i>=0;i--){
+            if(candidates[i]>target){
+                continue;
+            }else if(candidates[i]==target){
+                ArrayList<Integer> temp=new ArrayList<>();
+                temp.add(candidates[i]);
+                combinations.add(temp);
+            }else{
+                break;
+            }
+        }
         Stack<Integer> ress=new Stack<>();
 
-        sumOfkNumber(target,candidates,len,ress,combinations);
+        sumOfkNumber(target,candidates,i,ress,combinations);
         return combinations;
     }
     public void sumOfkNumber(int sum,int nums[],int n,Stack<Integer> res,List<List<Integer>> combinations){
@@ -59,10 +72,119 @@ public class CombinationSum {
 
     }
 
+
+    public void sumOfkNumber(int t,int k,int r,int target,boolean flag,boolean[] pickFlag){
+        pickFlag[k]=true;//选用第K个数
+
+        if(t+k==target){
+            flag=true;
+
+            for(int i=1;i<=k;i++){
+
+                if(pickFlag[i]==true){
+                    System.out.print(i+"->");
+                }
+
+
+            }
+            System.out.println();
+        }else {
+            if (t + k + (k + 1) <= target) {
+                sumOfkNumber(t + k, k + 1, r - k,target, flag, pickFlag);
+
+            }
+
+            if(((t+r-k)>=target) &&(t+k+1<=target)){
+                pickFlag[k]=false;
+                sumOfkNumber(t,k+1,r-k,target,flag,pickFlag);
+            }
+        }
+
+    }
+
+    public void search(int candidates,int target){
+
+
+
+
+        boolean[] pickFlag=new boolean[candidates+1];
+
+        boolean flag=false;
+        int sum=(int)(candidates*(candidates+1)*0.5);
+
+        sumOfkNumber(0,1,sum,target,flag,pickFlag);
+
+    }
+
+
+// leetcode 40
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> combinations=new ArrayList<>();
+        if(candidates==null || candidates.length==0){
+            return combinations;
+        }
+        Arrays.sort(candidates);
+        int len=candidates.length-1;
+
+        ArrayList<Integer> nums=new ArrayList<>();
+        ArrayList<Integer> counts=new ArrayList<>();
+
+        nums.add(candidates[0]);
+        counts.add(1);
+        int sizeOfNums=0;
+        for(int i=1;i<=len;i++){
+           if(nums.get(sizeOfNums)==candidates[i]){
+               counts.set(sizeOfNums,counts.get(sizeOfNums)+1);
+           }else if(candidates[i]<=target){
+               nums.add(candidates[i]);
+               counts.add(1);
+               sizeOfNums++;
+           }else{
+               break;
+           }
+        }
+        Stack<Integer> ress=new Stack<>();
+        sumOfkNumber(target,nums,counts,sizeOfNums,ress,combinations);
+        return combinations;
+    }
+
+    public void sumOfkNumber(int sum,List<Integer> nums,List<Integer> counts,int n,Stack<Integer> res,List<List<Integer>> combinations){
+        if(n<0||sum<=0){
+            return ;
+        }
+
+        if(sum==nums.get(n)){
+
+
+            ArrayList<Integer> temp=new ArrayList<Integer>();
+            temp.addAll(res);
+
+            temp.add(nums.get(n));
+            Collections.reverse(temp);
+            combinations.add(temp);
+
+        }
+
+        res.push(nums.get(n));
+        int tempSum=sum-nums.get(n);
+        if(counts.get(n)>1){
+            counts.set(n,counts.get(n)-1);
+            sumOfkNumber(tempSum,nums,counts,n,res,combinations);
+        }else{
+            counts.set(n,counts.get(n)-1);
+            sumOfkNumber(tempSum,nums,counts,n-1,res,combinations);
+        }
+
+        res.pop();
+        counts.set(n,counts.get(n)+1);
+        sumOfkNumber(sum,nums,counts,n-1,res,combinations);
+
+    }
+
     public static void main(String[] args) {
         CombinationSum com=new CombinationSum();
-        int[] nums={2,3,4};
-        List<List<Integer>> results=com.combinationSum(nums,7);
+        int[] nums={1,2};
+        List<List<Integer>> results=com.combinationSum2(nums,2);
 
         for(List<Integer> result:results){
             System.out.println(result);
