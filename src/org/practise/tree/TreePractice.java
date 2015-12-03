b32b7f19b89d0d26b2f04df2b79302157c97ea0e
djghostghost <xuxi746@163.com>
2015-12-03 20:43:05 +0900

    added leetcode #129

--- src/org/practise/tree/TreePractice.java
+++ src/org/practise/tree/TreePractice.java
@@ -9,26 +9,26 @@ import java.util.Stack;
  * Created by liguoxiang on 7/4/15.
  */
 public class TreePractice {
-//leetcode 94
-    public List<Integer> inorderTraversal(TreeNode root){
+    //leetcode 94
+    public List<Integer> inorderTraversal(TreeNode root) {
 
-        ArrayList<Integer> nodes=new ArrayList<>();
-        if(root==null){
+        ArrayList<Integer> nodes = new ArrayList<>();
+        if (root == null) {
             return nodes;
         }
 
-        Stack<TreeNode> stack=new Stack<>();
-        TreeNode p=root;
-        while(!stack.isEmpty()||p!=null){
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
 
-            if(p!=null){
+            if (p != null) {
                 stack.push(p);
-                p=p.left;
+                p = p.left;
 
-            }else{
-                p=stack.pop();
+            } else {
+                p = stack.pop();
                 nodes.add(p.val);
-                p=p.right;
+                p = p.right;
             }
 
 
@@ -38,25 +38,25 @@ public class TreePractice {
 
     }
 
-//  leetcode 144
+    //  leetcode 144
     public List<Integer> preorderTraversal(TreeNode root) {
-        LinkedList<Integer> ress=new LinkedList<>();
-        if(root==null){
+        LinkedList<Integer> ress = new LinkedList<>();
+        if (root == null) {
             return ress;
         }
 
-        Stack<TreeNode> stack=new Stack<>();
+        Stack<TreeNode> stack = new Stack<>();
 
-        TreeNode p=root;
-        while(!stack.isEmpty()||p!=null){
-            if(p!=null){
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
                 ress.add(p.val);
-                if(p.right!=null){
+                if (p.right != null) {
                     stack.push(p.right);
                 }
-                p=p.left;
-            }else{
-                p=stack.pop();
+                p = p.left;
+            } else {
+                p = stack.pop();
             }
         }
 
@@ -64,27 +64,27 @@ public class TreePractice {
 
     }
 
-//  leetcode 145
+    //  leetcode 145
     public List<Integer> postorderTraversal(TreeNode root) {
-        LinkedList<Integer> ress=new LinkedList<>();
-        if(root==null){
+        LinkedList<Integer> ress = new LinkedList<>();
+        if (root == null) {
             return ress;
         }
 
-        TreeNode lastVistedNode=null;
-        Stack<TreeNode> stack=new Stack<>();
-        while(!stack.isEmpty() || root!=null){
+        TreeNode lastVistedNode = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
 
-            if(root!=null){
+            if (root != null) {
                 stack.push(root);
-                root=root.left;
-            }else{
-                TreeNode peekNode=stack.peek();
-                if(peekNode.right!=null && lastVistedNode!=peekNode.right){
-                    root=peekNode.right;
-                }else{
+                root = root.left;
+            } else {
+                TreeNode peekNode = stack.peek();
+                if (peekNode.right != null && lastVistedNode != peekNode.right) {
+                    root = peekNode.right;
+                } else {
                     ress.add(peekNode.val);
-                    lastVistedNode=stack.pop();
+                    lastVistedNode = stack.pop();
                 }
             }
 
@@ -93,16 +93,15 @@ public class TreePractice {
         return ress;
 
 
-
     }
 
-    public boolean isBalanced(TreeNode root){
-        if(root==null){
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
             return true;
         }
 
 
-        if(Math.abs(getHeight(root.right)-getHeight(root.left))>1){
+        if (Math.abs(getHeight(root.right) - getHeight(root.left)) > 1) {
             return false;
         }
 
@@ -111,55 +110,81 @@ public class TreePractice {
     }
 
 
-    public int getHeight(TreeNode root){
-        if(root==null){
+    public int getHeight(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        String aa="";
-        return Math.max(1+getHeight(root.left),
-                        1+getHeight(root.right));
+        String aa = "";
+        return Math.max(1 + getHeight(root.left),
+                1 + getHeight(root.right));
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        if(root==null){
+        if (root == null) {
             return root;
         }
 
-        TreeNode p1=new TreeNode(Math.min(p.val,q.val));
-        TreeNode q1=new TreeNode(Math.max(p.val,q.val));
+        TreeNode p1 = new TreeNode(Math.min(p.val, q.val));
+        TreeNode q1 = new TreeNode(Math.max(p.val, q.val));
 
-        if(p1.val==root.val || q1.val==root.val) {
+        if (p1.val == root.val || q1.val == root.val) {
             return root;
-        }else if(p1.val<root.val && q1.val>root.val){
+        } else if (p1.val < root.val && q1.val > root.val) {
             return root;
-        }else if(p1.val<root.val && q1.val<root.val){
-            return lowestCommonAncestor(root.left,p1,q1);
-        }else{
-            return lowestCommonAncestor(root.right,p1,q1);
+        } else if (p1.val < root.val && q1.val < root.val) {
+            return lowestCommonAncestor(root.left, p1, q1);
+        } else {
+            return lowestCommonAncestor(root.right, p1, q1);
         }
 
     }
-//    leetcode #257
-public List<String> binaryTreePaths(TreeNode root) {
-    List<String> ress=new ArrayList<>();
-    if(root==null){
+
+    //    leetcode #257
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ress = new ArrayList<>();
+        if (root == null) {
+            return ress;
+        }
+
+        binaryTreePathsHelper(root, "", ress);
         return ress;
     }
 
-    binaryTreePathsHelper(root,"",ress);
-    return ress;
-}
-
-    public void binaryTreePathsHelper(TreeNode root,String path,List<String> ress){
-        if(root.left==null && root.right==null){
-            path+=root.val;
+    public void binaryTreePathsHelper(TreeNode root, String path, List<String> ress) {
+        if (root.left == null && root.right == null) {
+            path += root.val;
             ress.add(path);
             return;
         }
 
-        if(root.left!=null) binaryTreePathsHelper(root.left,path+root.val+"->",ress);
-        if(root.right!=null) binaryTreePathsHelper(root.right,path+root.val+"->",ress);
+        if (root.left != null) binaryTreePathsHelper(root.left, path + root.val + "->", ress);
+        if (root.right != null) binaryTreePathsHelper(root.right, path + root.val + "->", ress);
+
+    }
+
+    // leetcode 129
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        List<String> paths=new ArrayList<>();
+        int sum[]={0};
+        sumRootToLeaf(root,"",sum);
+
+        return sum[0];
+
+    }
+
+
+    public void sumRootToLeaf(TreeNode root,String path,int[] sum){
+
+        if(root.right==null && root.left==null){
+            path+=root.val;
+            sum[0]+=Integer.parseInt(path);
+            return;
+        }
+
+        if(root.left!=null) sumRootToLeaf(root.left,path+root.val,sum);
+        if(root.right!=null) sumRootToLeaf(root.right,path+root.val,sum);
 
     }
 
