d982b9c58023988fec37320e118b5fe473a9ecc3
djghostghost <xuxi746@163.com>
2015-12-13 20:09:33 +0900

    added leetcode #292

---
+++ src/org/practise/math/NimGame.java
@@ -0,0 +1,18 @@
+package org.practise.math;
+
+/**
+ * Author: ghost
+ * Date  : 2015-12-12
+ * TIME  : 22:08
+ * PACKAGE:org.practise.math
+ * leetcode 292
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+
+
+        return n>>2<<2!=n;
+
+
+    }
+}
