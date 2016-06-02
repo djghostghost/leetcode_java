6e7cbb6597492bccc2adc07d23d6a196e84276fb
@djghostghost <xuxi746@163.com>
2016-06-02 22:22:41 +0900

    add leetcode 300

---
+++ src/org/practise/DynamicProgramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,11 @@
+package org.practise.DynamicProgramming;
+
+/**
+ * Created by liguoxiang on 6/2/16.
+ * leetcode 300
+ */
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        return 0;
+    }
+}
