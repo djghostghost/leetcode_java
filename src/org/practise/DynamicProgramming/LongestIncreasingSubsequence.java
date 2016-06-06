bcaca2a7bc73e2ff56e23d82f787b9859d57eb24
@djghostghost <xuxi746@163.com>
2016-06-06 21:42:09 +0900

    update leetcode 300

--- src/org/practise/DynamicProgramming/LongestIncreasingSubsequence.java
+++ src/org/practise/DynamicProgramming/LongestIncreasingSubsequence.java
@@ -6,6 +6,23 @@ package org.practise.DynamicProgramming;
  */
 public class LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] longestSub = new int[nums.length];
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            longestSub[i] = 1;
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+                    longestSub[i] = longestSub[i] > longestSub[j] + 1 ? longestSub[i] : longestSub[j] + 1;
+                }
+            }
+            max = Math.max(max, longestSub[i]);
+        }
+
+        return max;
     }
 }
