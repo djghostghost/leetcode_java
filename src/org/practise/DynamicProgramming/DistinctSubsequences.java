5bf50c4519bae8606fa0b16e75ea883ff5cb97e8
@djghostghost <xuxi746@163.com>
2016-06-07 21:03:39 +0900

    add leetcode 115

---
+++ src/org/practise/DynamicProgramming/DistinctSubsequences.java
@@ -0,0 +1,14 @@
+package org.practise.DynamicProgramming;
+
+/**
+ * Created by liguoxiang on 6/7/16.
+ * leetcode 115
+ */
+public class DistinctSubsequences {
+    public int numDistinct(String s, String t) {
+        if(s==null||s.length()==0){
+            return 0;
+        }
+
+    }
+}
